f1b9d718986dc102cf1a064aafc7874080fdfa89
puregardenia <puregardenia@qq.com>
2016-07-12 13:58:36 +0800

    安装mongodb、session所需要的依赖

--- blog/app.js
+++ blog/app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
+var settings = require('./settings');
 
 var app = express();
 
@@ -22,8 +23,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// app.use('/', routes);
+// app.use('/users', users);
+routes(app);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ blog/models/db.js
@@ -0,0 +1,9 @@
+var settings = require('../settings'),
+    Db = require('mongodb').Db,
+    Connection = require('mongodb').Connection,
+    Server = require('mongodb').Server;
+module.exports = new Db(
+    settings.db,
+    new Server(setting.host, settings.port),
+    {safe: true}
+);
