764395e202e1572dedd8c5c74cc48ec3d3086c7d
puregardenia@qq.com <puregardenia@qq.com>
2016-10-28 17:00:35 +0800

    完成第一章

---
+++ SocketIO/app.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+var port = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, '/static')))
+
+app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, './static/index.html'))
+})
+
+var server = app.listen(port, function() {
+    console.log('TechNode  is on port ' + port + '!')
+})
+
+var io = require('socket.io').listen(server)
+
+var messages = []
+
+io.sockets.on('connection', function(socket) {
+    //socket.emit('connected');     // 测试数据
+    socket.on('messages.read', function() {
+        socket.emit('messages.read', messages)
+    })
+    socket.on('messages.create', function(message) {
+        messages.push(message)
+        io.sockets.emit('messages.add', message)
+    })
+})
